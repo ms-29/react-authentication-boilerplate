bd22c5c53c5cc6473665cda92c0e836ac4d9aae2
Mahedi Sabuj <mahedi.cse.net@gmail.com>
2018-12-14 20:58:54 +0600

    integrate apollo provider

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { ApolloProvider } from 'react-apollo';
+import { ApolloClient } from 'apollo-client';
+import { createHttpLink } from 'apollo-link-http';
+import { InMemoryCache } from "apollo-cache-inmemory";
 
-import App from './App';
+import App from './app/App';
 
-ReactDOM.render(<App />, 
+const httpLink = createHttpLink({
+  uri: process.env.REACT_APP_API
+});
+
+const cache = new InMemoryCache();
+
+const client = new ApolloClient({
+  link: httpLink,
+  cache: cache
+});
+
+ReactDOM.render(
+  <ApolloProvider client={client}>
+    <App />
+  </ApolloProvider>, 
   document.getElementById('root')
 );
