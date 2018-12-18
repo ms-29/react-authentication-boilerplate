3ab69ef77eff3c129356e464c7bc3f640c4dcc60
Mahedi Sabuj <mahedi.cse.net@gmail.com>
2018-12-18 12:47:46 +0600

    set up nodejs using babel

---
+++ api/server.js
@@ -0,0 +1,11 @@
+import express from "express";
+
+import "dotenv/config";
+
+const app = express();
+
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => {
+  console.log(`App is running at ${port}`);
+});
