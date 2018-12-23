d4c9c446fcf67906bb9ebf164a8f3c8834db4792
Mahedi Sabuj <mahedi.cse.net@gmail.com>
2018-12-23 15:05:05 +0600

    configure postgraphile

--- api/src/server.js
+++ api/src/server.js
@@ -1,8 +1,25 @@
 import express from 'express';
+import { postgraphile } from 'postgraphile';
+import { Pool } from 'pg';
 
 import 'dotenv/config';
 
 const app = express();
+
+const pgPool = new Pool({
+  user: process.env.POSTGRES_USER_NAME,
+  password: process.env.POSTGRES_PASSWORD,
+  host: process.env.POSTGRES_HOST,
+  port: process.env.POSTGRES_PORT,
+  database: process.env.POSTGRES_DATABASE
+});
+
+app.use(postgraphile(
+  pgPool, process.env.POSTGRES_DATABASE_SCHEMA, {
+    watchPg: true,
+    graphiql: true
+  }
+));
 
 const port = process.env.PORT || 3000;
 
