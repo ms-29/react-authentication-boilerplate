33ffc742d117af259629f5b1245e0bb3e208aecc
Mahedi Sabuj <mahedi.cse.net@gmail.com>
2019-01-09 18:31:48 +0600

    expose register_user api

---
+++ api/sql/user.sql
@@ -0,0 +1,34 @@
+-- Create user Table
+CREATE TABLE auth_public.user (
+  user_id SERIAL NOT NULL PRIMARY KEY,
+  email TEXT NOT NULL UNIQUE,
+  password_hash TEXT NOT NULL
+);
+
+-- Smart Comment
+COMMENT ON COLUMN auth_public.user.password_hash is E'@omit';
+COMMENT ON TABLE auth_public.user is E'@omit create';
+
+-- Register User
+CREATE FUNCTION auth_public.register_user(
+  email TEXT,
+  password TEXT
+) RETURNS auth_public.user AS $$
+DECLARE
+  new_user auth_public.user;
+BEGIN
+  INSERT INTO auth_public.user
+	(
+	  email,
+	  password_hash
+	)
+	VALUES
+	(
+	  email,
+	  CRYPT(password, GEN_SALT('bf'))
+	)
+  RETURNING * INTO new_user;
+				  
+  RETURN new_user;
+END
+$$ LANGUAGE PLPGSQL STRICT SECURITY DEFINER;
