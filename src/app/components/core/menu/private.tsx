6831251a3e294af4c7b8db7e8485c82bf95fc02b
Mahedi Sabuj <mahedi.cse.net@gmail.com>
2019-02-06 16:03:42 +0600

    show current logged in user

--- src/app/components/core/menu/private.tsx
+++ src/app/components/core/menu/private.tsx
@@ -1,15 +1,35 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { Query } from 'react-apollo';
+
+import { LOGGED_IN_USER } from './query';
+
+interface IResponse {
+  viewer: {
+    userId: number;
+    email: string;
+  }
+}
 
 class Private extends React.Component {
   render() {
     return (
       <ul className='nav navbar-nav ml-auto'>
-        <li className='nav-item'>
-          <Link className='nav-link' to='/Login'>
-            Logout
-          </Link>
-        </li>
+        <Query<IResponse> query={LOGGED_IN_USER}>
+          {({ data: response }) => {
+            if(response && Object.keys(response).length > 0) {
+              return (
+                <li className='nav-item'>
+                  <Link className='nav-link' to='/'>
+                    {response.viewer.email}
+                  </Link>
+                </li>
+              );
+            }
+
+            return '';
+          }}
+        </Query>
       </ul>
     );
   }  
