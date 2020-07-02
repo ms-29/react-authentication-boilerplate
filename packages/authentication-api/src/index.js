4ca46de6b05749f79d0d1a09045f6a57cb367ac6
Mahedi Sabuj <sabuj.ict.mbstu@gmail.com>
2020-07-02 15:15:03 +0600

    lerna add local dependencies

--- packages/authentication-api/src/index.js
+++ packages/authentication-api/src/index.js
@@ -1,42 +1,27 @@
-const express = require('express');
 const { Pool } = require('pg');
-const cors = require('cors');
 const { postgraphile } = require('postgraphile');
-const { parsed : config } = require('dotenv').config();
 const PgSimplifyInflectorPlugin = require('@graphile-contrib/pg-simplify-inflector');
 
-const app = express();
-
-const pgPool = new Pool({
-  user: config.POSTGRES_USER_NAME,
-  password: config.POSTGRES_PASSWORD,
-  host: config.POSTGRES_HOST,
-  port: config.POSTGRES_PORT,
-  database: config.POSTGRES_DATABASE
-});
-
-app.use(cors());
-
-app.use(postgraphile(
-  pgPool, config.POSTGRES_DATABASE_SCHEMA, {
-    graphiql: true,
-    watchPg: true,
-    showErrorStack: true,
-    jwtSecret: config.POSTGRES_JWT_SECRET,
-    jwtPgTypeIdentifier: config.POSTGRES_TYPE_IDENTIFIER,
-    extendedErrors: ['hint', 'detail', 'errcode'],
-    appendPlugins: [
-      PgSimplifyInflectorPlugin
-    ]
-  }
-));
-
-app.get("/*", function(request, response) {
-  response.send('Hello from Authentication API');
-});
-
-const PORT = config.PORT || 4000;
-
-app.listen(PORT, () => {
-  console.log(`App is Running at ${PORT}`);
-});
+module.exports = function (config) {
+  const pgPool = new Pool({
+    user: config.POSTGRES_USER_NAME,
+    password: config.POSTGRES_PASSWORD,
+    host: config.POSTGRES_HOST,
+    port: config.POSTGRES_PORT,
+    database: config.POSTGRES_DATABASE
+  });
+  
+  return postgraphile(
+    pgPool, config.POSTGRES_DATABASE_SCHEMA, {
+      graphiql: true,
+      watchPg: true,
+      showErrorStack: true,
+      jwtSecret: config.POSTGRES_JWT_SECRET,
+      jwtPgTypeIdentifier: config.POSTGRES_TYPE_IDENTIFIER,
+      extendedErrors: ['hint', 'detail', 'errcode'],
+      appendPlugins: [
+        PgSimplifyInflectorPlugin
+      ]
+    }
+  );
+};
--- packages/authentication-ui/server.js
+++ packages/authentication-ui/server.js
@@ -1,10 +1,14 @@
 const express = require('express');
 const path = require('path');
+const { parsed : config } = require('dotenv').config();
+const postgraphile = require('@ms-29/authentication-api');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(express.static(path.join(__dirname, 'build')));
+
+app.use(postgraphile(config));
 
 app.get("/*", function(request, response) {
   response.sendFile(path.join(__dirname, "build", "index.html"));
